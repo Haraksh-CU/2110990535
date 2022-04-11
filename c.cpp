fdf03129459a2d91f836b67cb558c884897ee6f3
Gurpreet2720 <gurpreet0535.be21@chitkara.edu.in>
2022-04-11 09:32:01 +0530

    Sum Of 3 number

---
+++ c.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num1;
+    int num2;
+    int num3;
+    int sum;
+    cout<<"Enter the first number:";
+    cin>>num1;
+    cout<<"Enter sec number:";
+    cin>>num2;
+    cout<<"Enter sec number:";
+    cin>>num3;
+    cout<<"Enter third number:";
+    sum = num1 + num2 + num3;
+    cout<<"The sum is: "<<sum<<endl;
+    return 0;
+}
